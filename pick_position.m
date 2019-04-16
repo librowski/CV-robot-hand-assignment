2ad2571258f7a501978eec579ebb5f7080be3cb0
vienmoir <daria.korotayeva@yandex.ru>
2019-04-16 14:01:47 +0300

    code for marking points on the image
    also a random image

---
+++ pick_position.m
@@ -0,0 +1,21 @@
+%%
+% grabImage = 'CMD_GRAB_IMAGE';
+% 
+% image = RV3SB_client(grabImage);
+clear all;
+[imname, pathname] = uigetfile({'*.jpg;*.tif;*.png;*.gif','All Image Files';...
+          '*.*','All Files' },'Выберите изображение');
+path = fullfile(pathname,imname);
+image = imread(path);
+
+
+figure
+imshow(image)
+[x,y] = getpts;
+XY(:,1) = x;
+XY(:,2) = y;
+save('xy.mat', 'XY');
+
+%%
+clear all;
+load('xy.mat');
